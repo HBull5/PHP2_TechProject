e5f1dc4efdd6bfd226776f3bd5a17481d91c761f
HBull5 <Harcourtbullv@gmail.com>
2020-01-15 08:23:43 -0500

    small update **not properly working**

--- model/technician_db.php
+++ model/technician_db.php
@@ -1,7 +1,13 @@
 <?php 
 
 function getTechs() {
-
+    global $db;
+    $query = 'SELECT * FROM technicians';
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $technicians = $statment->fetchAll();
+    $statement->closeCurosr();
+    return $technicians;
 }
 
 function deleteTech() {
--- tech_mananger/technician_list.php
+++ tech_mananger/technician_list.php
@@ -1,3 +1,20 @@
 <?php include '../view/header.php' ?>
-
+<div class="main">
+<h1>Technician List</h1>
+<table>
+    <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Phone</th>
+        <th>Password</th>
+        <th>&nbsp;</th>
+    </tr>
+    <?php foreach($techs as $tech) : ?>
+    <tr>
+        <td>$tech['firstName']; </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+</div>
 <?php include '../view/footer.php' ?>
