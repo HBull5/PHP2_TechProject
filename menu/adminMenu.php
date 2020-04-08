4c9392a40e81fab767676f4c3abb8fffbed4cb54
HBull5 <harcourtbullv@gmail.com>
2020-04-08 16:43:12 -0400

    added admin logout

--- menu/adminMenu.php
+++ menu/adminMenu.php
@@ -1,4 +1,7 @@
-<?php include '../view/header.php' ?>
+<?php 
+include '../view/header.php'; 
+session_start();
+?>
 <div id="main">
     <h2>Admin Menu</h2>
     <ul class="nav">
@@ -9,5 +12,10 @@
         <li><a href="../incident_assigner">Assign Incident</a></li>
         <li><a href="../incident_viewer">Display Incidents</a></li>
     </ul>
+    <form action="." method="post">
+        <input type="hidden" name="action" value="logout">
+        <p>You are logged in as <?php echo $_SESSION['loginID']['username'] ?></p>
+        <input type="submit" value="Logout">
+    </form>
 </div>
 <?php include '../view/footer.php' ?>
--- menu/index.php
+++ menu/index.php
@@ -1,6 +1,7 @@
 <?php 
 session_start();
 $loginID = $_SESSION['loginID'];
+$action = filter_input(INPUT_POST, 'action');
 if(array_key_exists('username', $loginID)) {
     $menuType = 'admin';
 } else if(array_key_exists('techID', $loginID)) {
@@ -9,15 +10,21 @@ if(array_key_exists('username', $loginID)) {
     $menuType = 'customer';
 }
 
-switch($menuType) {
-    case 'admin':
-        header("Location: adminMenu.php");
-        break;
-    case 'tech':
-        header("Location: ../incident_updater");
-        break;
-    case 'customer':
-        header("Location: ../registration_manager");
-        break;
+if($action === 'logout') {
+    unset($_SESSION['loginID']);
+    header("Location: ../login/login.php?type=admin");
+} else {
+    switch($menuType) {
+        case 'admin':
+            header("Location: adminMenu.php");
+            break;
+        case 'tech':
+            header("Location: ../incident_updater");
+            break;
+        case 'customer':
+            header("Location: ../registration_manager");
+            break;
+    }
 }
+
 ?>
