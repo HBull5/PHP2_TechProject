e9275dacee5a49544133b50716ba0eaa1489f794
HBull5 <harcourtbullv@gmail.com>
2020-04-08 20:51:24 -0400

    Added functionalilty for a tech with no incidents assigned

--- incident_updater/select_incident.php
+++ incident_updater/select_incident.php
@@ -19,30 +19,35 @@ if(!isset($_SESSION['validated'])) {
 ?>
 <div id="main">
     <h1>Select Incident</h1>
-    <table>
-        <tr>
-            <th>Customer</th>
-            <th>Product</th>
-            <th>Date Opened</th>
-            <th>Title</th>
-            <th>Description</th>
-            <th>&nbsp;</th>
-        </tr>
-    <?php foreach($incidents as $incident) : ?>
-        <tr>
-            <td><?php echo $incident['fullName'] ?></td>
-            <td><?php echo $incident['productCode'] ?></td>
-            <td><?php echo dateFormatter($incident['dateOpened']) ?></td>
-            <td><?php echo $incident['title'] ?></td>
-            <td><?php echo $incident['description'] ?></td>
-            <td><form action="." method="post">
-                <input type="hidden" name="action" value="update">
-                <input type="hidden" name="incidentID" value="<?php echo $incident['incidentID'] ?>">
-                <input type="submit" value="Select">
-            </form></td>
-        </tr>
-    <?php endforeach; ?>
-    </table>
+    <?php if(empty($incidents)) : ?>
+        <p>There are no open incidents for this technician</p>
+        <p><a href="select_incident.php">Refresh List of Incidents</a></p>
+    <?php else : ?>
+        <table>
+            <tr>
+                <th>Customer</th>
+                <th>Product</th>
+                <th>Date Opened</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>&nbsp;</th>
+            </tr>
+        <?php foreach($incidents as $incident) : ?>
+            <tr>
+                <td><?php echo $incident['fullName'] ?></td>
+                <td><?php echo $incident['productCode'] ?></td>
+                <td><?php echo dateFormatter($incident['dateOpened']) ?></td>
+                <td><?php echo $incident['title'] ?></td>
+                <td><?php echo $incident['description'] ?></td>
+                <td><form action="." method="post">
+                    <input type="hidden" name="action" value="update">
+                    <input type="hidden" name="incidentID" value="<?php echo $incident['incidentID'] ?>">
+                    <input type="submit" value="Select">
+                </form></td>
+            </tr>
+        <?php endforeach; ?>
+        </table>
+    <?php endif; ?>
     <form action="." method="post">
         <input type="hidden" name="action" value="logout">
         <p>You are logged in as <?php echo $email ?></p>
